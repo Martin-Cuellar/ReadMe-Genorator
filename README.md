0fee225588f01b978b95aa36e5077ca40532ac43
Martin Cuellar <martinhc129@gmail.com>
2023-02-13 18:39:29 -0500

    finished all the todo's

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# ReadMe-Genorator
+# readme genorator
+  ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+## Description
+
+to generate a readme file
+
+## Table of Contents 
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+```
+none 
+```
+
+## Usage
+
+
+
+## License
+
+This project is licensed under the MIT license.
+  
+## Contributing
+
+
+
+## Tests
+
+To run tests, run the following command:
+
+```
+undefined
+```
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at . You can find more of my work at [wj](https://github.com/wj/).
+
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -65,10 +68,16 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+  }
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((inquirerResponses) => {
+        console.log('Generating README...');
+        writeToFile('README.md', generateMarkdown({ ...inquirerResponses }));
+      });
+}
 
 // Function call to initialize app
 init();
